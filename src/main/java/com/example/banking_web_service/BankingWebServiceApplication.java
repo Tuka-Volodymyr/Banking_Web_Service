21f88742e58c8749c0741ec15603d2c35a6bfe5d
Volodymyr <tukavolody@gmail.com>
2023-08-15 17:58:34 +0300

    Add main class

---
+++ src/main/java/com/example/banking_web_service/BankingWebServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.banking_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingWebServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankingWebServiceApplication.class, args);
+	}
+}
