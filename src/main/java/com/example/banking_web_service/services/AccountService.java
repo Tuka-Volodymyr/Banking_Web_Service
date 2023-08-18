fc0f1e5aae3801721904362077b9b0f80a6cfe1a
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:36:20 +0300

    Add to account service lock methods

--- src/main/java/com/example/banking_web_service/services/AccountService.java
+++ src/main/java/com/example/banking_web_service/services/AccountService.java
@@ -1,28 +1,67 @@
 package com.example.banking_web_service.services;
 import com.example.banking_web_service.data.AccountData;
+import com.example.banking_web_service.dto.CreditCardDto;
 import com.example.banking_web_service.entities.Account;
 import com.example.banking_web_service.entities.CreditCard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
 
 @Service
 public class AccountService {
     @Autowired
     private AccountData accountData;
-    public ResponseEntity<?> addAccount(Account account){
+    @Autowired
+    private EventService eventService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String addAccount(Account account){
+        account.setLockStatus(false);
+        account.setFailedAttempts(0);
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        accountData.existAccount(account);
         accountData.saveAccount(account);
-        return new ResponseEntity<>("Account was created by "+ account.getFullName(), HttpStatus.CREATED);
+        eventService.createAccount(account.getEmail());
+        return "Account was created by "+ account.getFullName();
     }
-    public ResponseEntity<?> addCreditCard(UserDetails userDetails){
-        Account account=accountData.findAccountByEmail(userDetails.getUsername());
-        CreditCard creditCard=new CreditCard();
-        creditCard.setEmailOfAccount(account.getEmail());
-        creditCard.createCreditCard();
-        accountData.saveCreditCard(creditCard);
-        return new ResponseEntity<>(creditCard,HttpStatus.CREATED);
+    public void resetAccountAttempts(String email){
+        Account account=accountData.findAccountByEmail(email);
+        account.setFailedAttempts(0);
+        accountData.saveAccount(account);
     }
 
+    public void increaseFailedAttempts(Account account){
+        eventService.authenticationFailed(account.getEmail());
+        account.setFailedAttempts(account.getFailedAttempts()+1);
+        accountData.saveAccount(account);
+    }
+    public String lockAccount(String email) {
+        Account account = accountData.findAccountByEmail(email);
+        if (!account.isLockStatus()) {
+            account.setLockStatus(true);
+            account.setFailedAttempts(0);
+            accountData.saveAccount(account);
+            eventService.lock(email);
+            return account.getFullName() + " your account is lock!";
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Account don`t lock!");
+    }
+    public String unlockAccount(String email){
+        Account account=accountData.findAccountByEmail(email);
+        if(account.isLockStatus()){
+            account.setLockStatus(false);
+            account.setFailedAttempts(0);
+            accountData.saveAccount(account);
+            eventService.unlock(email);
+            return account.getFullName()+" your account is unlock!";
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Account don`t lock!");
+    }
 }
