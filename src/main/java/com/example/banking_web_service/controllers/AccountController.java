81b7150e22a245b779460381c8a041e41314a935
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:32:11 +0300

    Add to account controller new requests

--- src/main/java/com/example/banking_web_service/controllers/AccountController.java
+++ src/main/java/com/example/banking_web_service/controllers/AccountController.java
@@ -4,13 +4,9 @@ import com.example.banking_web_service.entities.Account;
 import com.example.banking_web_service.services.AccountService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
@@ -18,12 +14,14 @@ public class AccountController {
     private AccountService accountService;
     @PostMapping("/bank/addAccount")
     public ResponseEntity<?> addAccount(@Valid @RequestBody Account account){
-        return accountService.addAccount(account);
+        return new ResponseEntity<>(accountService.addAccount(account), HttpStatus.CREATED);
     }
-    @PostMapping("/bank/addCard")
-    public ResponseEntity<?> addAccount(@AuthenticationPrincipal UserDetails userDetails){
-        return accountService.addCreditCard(userDetails);
+    @PostMapping("/bank/lock/account")
+    public ResponseEntity<?> lockAccount(@RequestParam String email){
+        return new ResponseEntity<>( accountService.lockAccount(email),HttpStatus.OK);
     }
-
-
+    @PostMapping("/bank/unlock/account")
+    public ResponseEntity<?> unlockAccount(@RequestParam String email){
+        return new ResponseEntity<>(accountService.unlockAccount(email), HttpStatus.OK);
+    }
 }
