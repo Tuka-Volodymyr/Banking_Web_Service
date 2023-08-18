4b081dd17ad3486bf1aababe80936a6323017749
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:53:02 +0300

    Update SecurityConfiguration class

--- src/main/java/com/example/banking_web_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/banking_web_service/configuration/SecurityConfiguration.java
@@ -1,22 +1,21 @@
 package com.example.banking_web_service.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Service;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
-
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -26,7 +25,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf().disable()
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/bank/addAccount","/error/**").permitAll()
+                        .requestMatchers("/bank/addAccount","/error/**","/bank/card/balance/add/money").permitAll()
                         .requestMatchers("/bank/**").authenticated())
                 .httpBasic()
                 .and()
