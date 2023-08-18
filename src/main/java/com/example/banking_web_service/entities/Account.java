a42be445d4e08f1b7f3b0b8cf4885b2ec17d7af3
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:31:05 +0300

    Add to account entity class new variables

--- src/main/java/com/example/banking_web_service/entities/Account.java
+++ src/main/java/com/example/banking_web_service/entities/Account.java
@@ -9,15 +9,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 
 
-@Setter
-@Getter
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +31,12 @@ public class Account {
     private String email;
     @Size(min = 4)
     private String password;
+    @JsonIgnore
+    private boolean lockStatus;
+    @JsonIgnore
+    private int failedAttempts;
+
+
 
 
 //    private HashSet<String> role;
