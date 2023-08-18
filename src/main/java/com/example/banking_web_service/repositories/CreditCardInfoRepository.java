3d2016f1d9210892e72981ac710871d957fb4fb0
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:42:44 +0300

    Add repository for work with database

--- src/main/java/com/example/banking_web_service/repositories/CreditCardInfoRepository.java
+++ src/main/java/com/example/banking_web_service/repositories/CreditCardInfoRepository.java
@@ -1,10 +1,15 @@
 package com.example.banking_web_service.repositories;
 
+import com.example.banking_web_service.entities.Account;
 import com.example.banking_web_service.entities.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CreditCardInfoRepository extends JpaRepository<CreditCard,Long> {
+    Optional<CreditCard> findByEmailOfOwner(String email);
+    Optional<CreditCard> findByCard(String card);
 
 }
