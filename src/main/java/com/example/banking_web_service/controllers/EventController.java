ba01dea4e45ee3c3a5f533563728d0e666dc5428
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:44:16 +0300

    Add event controller with requests

---
+++ src/main/java/com/example/banking_web_service/controllers/EventController.java
@@ -0,0 +1,24 @@
+package com.example.banking_web_service.controllers;
+
+import com.example.banking_web_service.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    @Autowired
+    private EventService eventService;
+    @GetMapping("/bank/events")
+    public ResponseEntity<?> getGlobalEvents(){
+        return new ResponseEntity<>(eventService.getGlobalEvents(), HttpStatus.OK);
+    }
+    @GetMapping("/bank/history")
+    public ResponseEntity<?> getHistoryTransaction(@AuthenticationPrincipal UserDetails userDetails){
+        return new ResponseEntity<>(eventService.getOperationEvents(userDetails), HttpStatus.OK);
+    }
+}
