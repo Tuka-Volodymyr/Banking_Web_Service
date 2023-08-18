cd05af657b986143892a4f79e4c788fe1929de86
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:33:48 +0300

    Update account data class

--- src/main/java/com/example/banking_web_service/data/AccountData.java
+++ src/main/java/com/example/banking_web_service/data/AccountData.java
@@ -3,41 +3,38 @@ package com.example.banking_web_service.data;
 import com.example.banking_web_service.entities.Account;
 import com.example.banking_web_service.entities.CreditCard;
 import com.example.banking_web_service.exceptions.AccountNotFoundException;
+import com.example.banking_web_service.exceptions.CreditCardNotFoundException;
 import com.example.banking_web_service.repositories.AccountInfoRepository;
 import com.example.banking_web_service.repositories.CreditCardInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Component
 public class AccountData {
     @Autowired
     private  AccountInfoRepository accountInfoRepository;
-    @Autowired
-    private CreditCardInfoRepository creditCardInfoRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
 
     public Account findAccountByEmail(String email){
         return accountInfoRepository
-                .findByEmail(email)
+                .findByEmailIgnoreCase(email)
                 .orElseThrow(AccountNotFoundException::new);
     }
     public void saveAccount(Account account){
-        existAccount(account);
-        account.setPassword(passwordEncoder.encode(account.getPassword()));
         accountInfoRepository.save(account);
     }
     public void existAccount(Account account){
-        Optional<Account> someAccount =accountInfoRepository.findByEmail(account.getEmail());
+        Optional<Account> someAccount =accountInfoRepository.findByEmailIgnoreCase(account.getEmail());
         if(someAccount.isPresent())throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Email has already used!");
     }
-    public void saveCreditCard(CreditCard creditCard){
-        creditCardInfoRepository.save(creditCard);
-    }
+
 
 }
