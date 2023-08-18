b005011a2c6acdaad7f53f67bd8d20aab0319c4f
Volodymyr <tukavolody@gmail.com>
2023-08-19 01:33:27 +0300

    Add main test class

---
+++ src/test/java/com/example/banking_web_service/BankingWebServiceApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.banking_web_service;
+
+import com.example.banking_web_service.data.AccountData;
+import com.example.banking_web_service.entities.Account;
+import com.example.banking_web_service.entities.CreditCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class BankingWebServiceApplicationTests {
+
+//	@Test
+//	void AccountDataFindByEmailTest() {
+//		CreditCard creditCard=new CreditCard();
+//		assertEquals(LocalDate.now(),creditCard.createCreditCard());
+//	}
+
+}
