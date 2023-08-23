1152888ee3977973099ad6a1a06ac870e76f1022
Volodymyr <tukavolody@gmail.com>
2023-08-23 15:36:52 +0300

    Add class for accept change password data

---
+++ src/main/java/com/example/banking_web_service/dto/ChangePassword.java
@@ -0,0 +1,19 @@
+package com.example.banking_web_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePassword {
+    @Email
+    @Pattern(regexp = ".+\\.com")
+    private String email;
+    @Size(min = 4)
+    private String password;
+}
