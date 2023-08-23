e146bdb6faa6cd50a76b213f8b8c30d301e92338
Volodymyr <tukavolody@gmail.com>
2023-08-23 15:31:38 +0300

    Update PrincipalDetailsArgumentResolver class

--- src/test/java/com/example/banking_web_service/configuration/PrincipalDetailsArgumentResolver.java
+++ src/test/java/com/example/banking_web_service/configuration/PrincipalDetailsArgumentResolver.java
@@ -1,22 +1,22 @@
 package com.example.banking_web_service.configuration;
 
-import com.example.banking_web_service.entities.Account;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+import static com.example.banking_web_service.services.AccountServiceTest.secondAccount;
+
 public class PrincipalDetailsArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().isAssignableFrom(AccountDetails.class);
     }
-
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        Account account = new Account(1L, "Tuka", "tuka@gmail.com", "1111");
-        AccountDetails accountDetails=new AccountDetails(account);
-        return accountDetails;
+        return new AccountDetails(secondAccount);
     }
 }
