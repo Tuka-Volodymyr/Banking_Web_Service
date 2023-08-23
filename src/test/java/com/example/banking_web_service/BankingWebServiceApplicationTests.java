c318f6bb0b2751f2c33bfa1d873d5e4a22988910
Volodymyr <tukavolody@gmail.com>
2023-08-23 15:25:19 +0300

    Added main test class

--- src/test/java/com/example/banking_web_service/BankingWebServiceApplicationTests.java
+++ src/test/java/com/example/banking_web_service/BankingWebServiceApplicationTests.java
@@ -1,9 +1,18 @@
 package com.example.banking_web_service;
 
+import com.example.banking_web_service.controllers.AccountControllerTest;
+import com.example.banking_web_service.controllers.CardControllerTest;
 import com.example.banking_web_service.data.AccountData;
 import com.example.banking_web_service.entities.Account;
 import com.example.banking_web_service.entities.CreditCard;
+import com.example.banking_web_service.services.AccountServiceTest;
+import com.example.banking_web_service.services.CardService;
+import com.example.banking_web_service.services.CardServiceTest;
+import junit.textui.TestRunner;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
@@ -12,13 +21,13 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AccountServiceTest.class,
+        CardServiceTest.class,
+        CardControllerTest.class,
+        AccountControllerTest.class
+})
 class BankingWebServiceApplicationTests {
-
-//	@Test
-//	void AccountDataFindByEmailTest() {
-//		CreditCard creditCard=new CreditCard();
-//		assertEquals(LocalDate.now(),creditCard.createCreditCard());
-//	}
 
 }
