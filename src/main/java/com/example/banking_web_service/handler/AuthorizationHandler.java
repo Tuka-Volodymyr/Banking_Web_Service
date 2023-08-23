415e7994a9d340b9de89bf33a1a4cb4b22c02584
Volodymyr <tukavolody@gmail.com>
2023-08-23 15:33:23 +0300

    Add class for throw 403 error

---
+++ src/main/java/com/example/banking_web_service/handler/AuthorizationHandler.java
@@ -0,0 +1,28 @@
+package com.example.banking_web_service.handler;
+
+
+import com.example.banking_web_service.services.EventService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+public class AuthorizationHandler implements AccessDeniedHandler {
+
+    @Autowired
+    private EventService eventService;
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException {
+        eventService.authorizationFailed();
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied!");
+
+    }
+}
